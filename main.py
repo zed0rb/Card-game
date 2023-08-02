c70606422cbe553f4147f43fb8ff3c485fe6ff2e
miglemr <113340648+miglemr@users.noreply.github.com>
2023-08-02 18:50:50 +0300

    Update main.py
    Add parent class 'Player' and child class 'PlayerHand'

--- main.py
+++ main.py
@@ -26,3 +26,26 @@ class Deck:
     # shuffle the deck
     def shuffle(self):
         return random.shuffle(self.cards)
+
+class Player:
+    def __init__(self, player_deck, name):
+        self.name = name
+        self.deck = player_deck
+
+    # Checks if player got cards in his deck
+    def has_cards(self):
+        return bool(self.deck)
+
+
+class PlayerHand(Player):
+    # returns player and number for cards in his deck
+    def __str__(self):
+        return "{} have {} cards".format(self.name, len(self.deck))
+
+    # add cards to player deck
+    def add_cards(self, add_cards):
+        return self.deck.extend(add_cards)
+
+    # remove 1 card from top of his deck
+    def play_card(self):
+        return self.deck.pop()
